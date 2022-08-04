9d80d70893aeed7d96b98c08530c2cb2b677c465
kangupdate1 <90378163+kangupdate1@users.noreply.github.com>
2022-08-05 03:17:20 +0700

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -54,8 +54,8 @@ def stats(update, context):
 
 def start(update, context):
     buttons = ButtonMaker()
-    buttons.buildbutton("Admin", "https://t.me/")
-    buttons.buildbutton("Group", "https://t.me/")
+    buttons.buildbutton("Admin", "https://t.me/+GTGd4_M68w0wZmJl")
+    buttons.buildbutton("Group", "https://t.me/+eMZmUYcvGqJmMWY1")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f''' 
